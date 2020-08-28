d3f328a8a11aeef1f5a67a0be7dab2fc5730a81b
prcummins <prcummins@gmail.com>
2020-08-28 16:29:08 -0700

    NewGolangTest

--- GoLang/main.go
+++ GoLang/main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main () {
-  fmt.Printf("Hello World")
+  // EDIT HERE
+  s := `Test`
+  fmt.Println(s)
+
+  bs := []byte(s)
+  fmt.Println(bs)
+  fmt.Printf("%T\n", bs)
 }
