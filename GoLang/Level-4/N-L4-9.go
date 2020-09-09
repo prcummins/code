f82073e849ad2863d917cbad97c30242263cd490
prcummins <prcummins@gmail.com>
2020-09-09 13:06:24 -0700

    Updated

---
+++ GoLang/Level-4/N-L4-9.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main () {
+  // EDIT HERE
+  m := map[string][]string{
+    `PC`: []string{`Hiking`, `Biking`, `Soccer`},
+    `JC`: []string{`Soccer`, `XBOX`, `Band`},
+    `WC`: []string{`LAX`, `XBOX`, `Youtube`},
+  }
+
+  m[`NB`] = []string{`Reading`, `Drinking`, `Walks`}
+    for i, v := range m{
+      fmt.Println("Record", i)
+        for i2, v2 := range v {
+          fmt.Printf("Index Position: %v\t Value: %v\n", i2, v2)
+        }
+    }
+}
