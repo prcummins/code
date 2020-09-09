7bc0f9d493d4f3dee892fddbd949d270b8966523
prcummins <prcummins@gmail.com>
2020-09-09 11:28:33 -0700

    Updated

---
+++ GoLang/Level-4/N-L4-3.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main () {
+  // EDIT HERE
+  s := []int{42, 43, 44, 45, 46, 47, 48, 49, 50, 51}
+  fmt.Println(s[:5])
+  fmt.Println(s[5:])
+  fmt.Println(s[2:7])
+  fmt.Println(s[1:6])
+}
