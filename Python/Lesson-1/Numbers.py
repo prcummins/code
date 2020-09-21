8cea4dc92f88dba0595a10d4af5584c0c50dd784
prcummins <prcummins@gmail.com>
2020-09-21 13:13:23 -0700

    Updaated

---
+++ Python/Lesson-1/Numbers.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+
+# PYTHON TRAINING
+# In Python there are three distinct numeric types:
+# Integers
+# Floating-point numbers
+# Complex numbers
+
+##### INTEGERS #####
+
+x = 10
+y = -10
+z = 123456789
+
+print(x, y, z)
+
+##### BINARY #####
+
+print(0b1011)
+
+##### OCTAL #####
+
+print(0o10)
+
+##### HEX #####
+
+print(0xFF)
+
+##### Boolean is a subtype of Integers #####
+
+x = True
+y = False
